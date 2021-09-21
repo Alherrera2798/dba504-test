549c842a848d545ef68e4a74f00a0cef92479629
Alejandro Herrera <a.herrera906@gmail.com>
2021-09-20 20:06:29 -0400

    Adding line of code!

--- test.py
+++ test.py
@@ -5,3 +5,11 @@ vartest = pd.read_csv('test.csv')
 vartest2 = pd.read_csv('test2.csv')
 
 #Victoria has entered the chat 
+
+
+students = ['Ashely', 'Yiyi', 'Alejandro']
+for student in students:
+	print (f{student.title()}', you guys did great!!')
+
+
+	
